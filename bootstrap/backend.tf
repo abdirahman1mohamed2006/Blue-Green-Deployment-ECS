91ee39ae13af28d62a670fa15ec68c1ec164597b
Abdirahman Mohamed <Abdirahman1mohamed2006@gmail.com>
2026-04-28 19:26:28 +0100

    Change in backend2.0

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "ecs-v2-terraform-state"
     key            = "terraform.tfstate"
-    region         = "eu-west-2"
+    region         = "eu-west-1"
     dynamodb_table = "dynamodb-table"
   }
 }
