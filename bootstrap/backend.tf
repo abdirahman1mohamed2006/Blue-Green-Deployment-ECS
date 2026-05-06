2016304e036e725bd9fadf147ed5aeb94d4a4ee8
Abdirahman Mohamed <Abdirahman1mohamed2006@gmail.com>
2026-05-06 22:11:05 +0100

    S3 Region2.0

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -10,3 +10,14 @@ terraform {
   }
 }
 
+data "terraform_remote_state" "bootstrap" {
+  backend = "s3"
+
+  config = {
+    bucket = "ecs-v2-terraform-state-848153448908"
+    key    = "bootstrap/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+
