3bdcc22df3cc65c0c368e9febf06d10f8ccec607
Abdirahman Mohamed <Abdirahman1mohamed2006@gmail.com>
2026-05-06 21:12:23 +0100

    VPC = Public subnet IDs

--- Terraform/modules/VPC/output.tf
+++ Terraform/modules/VPC/output.tf
@@ -4,12 +4,12 @@ output "vpc_id" {
 }
 
 output "public_subnet_1_id" {
-    value = aws_subnet.public_1
+    value = aws_subnet.public_1.id
   
 }
 
 output "public_subnet_2_id" {
-    value = aws_subnet.public_2
+    value = aws_subnet.public_2.id
   
 }
 
