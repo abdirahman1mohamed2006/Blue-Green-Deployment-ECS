5f155aaf1882bae0a5f4866cb02c09132f7c020c
Abdirahman Mohamed <Abdirahman1mohamed2006@gmail.com>
2026-05-07 03:29:58 +0100

    Created Route53 module1.0

--- Terraform/modules/ACM/output.tf
+++ Terraform/modules/ACM/output.tf
@@ -2,6 +2,3 @@ output "acm_certificate_arn" {
   value = aws_acm_certificate.cert.arn
 }
 
-output "validated_certificate_arn" {
-  value = aws_acm_certificate_validation.cert_validation.certificate_arn
-}
