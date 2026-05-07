35ed5ec2e8ae2b845b33301cedbcc3de44515118
Abdirahman Mohamed <Abdirahman1mohamed2006@gmail.com>
2026-05-07 03:36:06 +0100

    Created Route53 module1.0

--- Terraform/modules/Route53/main.tf
+++ Terraform/modules/Route53/main.tf
@@ -1,19 +1,4 @@
-resource "aws_route53_record" "validation" { 
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options :
-    dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
 
-  zone_id = var.alb_zone_id
-  name    = each.value.name
-  type    = each.value.type
-  ttl     = 60
-  records = [each.value.record]
-}
 
 data "aws_route53_zone" "zone_ecs2" { 
   name         = var.zone_name
