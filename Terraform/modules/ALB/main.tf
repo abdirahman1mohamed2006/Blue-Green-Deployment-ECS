30d28aeda4aa7256eb992afcb5d60631fa81b47e
Abdirahman Mohamed <Abdirahman1mohamed2006@gmail.com>
2026-05-10 03:34:39 +0100

    port mapping

--- Terraform/modules/ALB/main.tf
+++ Terraform/modules/ALB/main.tf
@@ -7,15 +7,15 @@ resource "aws_security_group" "alb_sg" {
 resource "aws_vpc_security_group_ingress_rule" "alb_http" {
   security_group_id = aws_security_group.alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  to_port           = 443
+  from_port         = 80
+  to_port           = 80
   ip_protocol       = "tcp"
 }                                                                 
 resource "aws_vpc_security_group_ingress_rule" "alb_httpinternet" {    
   security_group_id = aws_security_group.alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 5320
-  to_port           = 5320
+  from_port         = 443
+  to_port           = 443
   ip_protocol       = "tcp"
 }
 
