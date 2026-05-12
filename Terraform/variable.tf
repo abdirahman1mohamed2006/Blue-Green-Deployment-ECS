a84cd92a36d6c4ead154e7cd95d0dc84ed93fe7a
Abdirahman Mohamed <Abdirahman1mohamed2006@gmail.com>
2026-05-12 15:03:50 +0100

    Latest Image URL

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -49,7 +49,7 @@ variable "service_name" {
 
 variable "image" {
   type    = string
-  default = "848153448908.dkr.ecr.eu-west-1.amazonaws.com/ecsv2:latest"
+  default = "848153448908.dkr.ecr.eu-west-1.amazonaws.com/ecsv2repo:latest"
 }
 
 variable "app_name" {
